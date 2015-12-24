8ff70f016de1e3b597c7ebe793363aa4c2718883
massahud <massahud@gmail.com>
2015-12-24 19:50:36 -0200

    first version

---
+++ bingo/public_html/js/bingo.js
@@ -0,0 +1,42 @@
+var Bingo = function() {
+  
+  this.sorteados = [];
+  
+  this.sortear = function() {
+    var numero = this.ordemSorteio.splice(0,1)[0];
+  	this.sorteados.push(numero);
+    for (var i = 0; i < this.listeners.length; i++) {
+      this.listeners[i].onNumeroSorteado(numero, this);
+    }
+    return numero;
+  };
+  this.listeners = [];
+  this.addListener = function (listener) {
+		  this.listeners.push(listener);
+  };
+  this.getQtdNumerosFaltam = function() {
+    return this.ordemSorteio.length;
+  };
+  this.getQtdNumerosSorteados = function() {
+    return this.sorteados.length;
+  };
+  this.reiniciar = function() {
+    var numeros = [];
+    for (var i = 0; i < 75; i++) {
+      numeros[i] = i+1;
+    }
+    this.ordemSorteio = [];
+
+    for (var i = 75; i > 0; i--) {
+       var pos = Math.floor(Math.random() * i);
+       var numero = numeros.splice(pos,1)[0];
+       this.ordemSorteio.push(numero);
+    }
+  	for (var i = 0; i < this.listeners.length; i++) {
+      this.listeners[i].onReiniciado(this);
+    }  
+  }; 
+  this.reiniciar();
+  
+  return this;
+}	
---
+++ bingo/public_html/js/main.js
@@ -0,0 +1,57 @@
+var $divNumerosSorteados,
+    $divsNumeros,    
+    $ordemSorteio,
+    listenerNumerosSorteados = {},
+    listenerOrdemSorteio = {},
+    listenerExibir = {},
+    bingo,
+    $botaoSortear,
+    $numeroSorteado;
+
+listenerNumerosSorteados.onNumeroSorteado = function(numero, bingo) {
+    $($divsNumeros[numero-1]).addClass('sorteado');
+};
+
+listenerNumerosSorteados.onReiniciado = function(bingo) {
+    $divNumerosSorteados = $('#numerosSorteados');
+    $divsNumeros = [];
+    $divNumerosSorteados.html('');
+    for (var i = 0; i < 75; i++) {
+        $div = $('<div class="numero">' + (i+1) + '</div>');
+        $divsNumeros.push($div);
+        $divNumerosSorteados.append($div);
+    }
+};
+
+listenerOrdemSorteio.onNumeroSorteado = function(numero, bingo) {
+    $ordemSorteio.append($('<span class="ordemSorteio">' + numero + '</span>'));
+};
+
+listenerOrdemSorteio.onReiniciado = function(bingo) {
+    $ordemSorteio = $('#ordemSorteio');
+    $ordemSorteio.html('');
+};
+
+listenerExibir.onNumeroSorteado = function(numero, bingo) {
+    var el = $numeroSorteado.clone();
+    
+    el.text('' + numero);
+    $numeroSorteado.after(el);
+    el.addClass('animar');
+};
+
+listenerExibir.onReiniciado = function(numero, bingo) {
+    $numeroSorteado = $($('.numeroSorteado')[0]);
+}
+
+$(document).ready(function(){
+    bingo = new Bingo();
+    bingo.addListener(listenerNumerosSorteados);
+    bingo.addListener(listenerOrdemSorteio);
+    bingo.addListener(listenerExibir);
+    bingo.reiniciar();
+    $botaoSortear = $('#sortear');
+    $botaoSortear.click(function() {
+       bingo.sortear(); 
+    });
+});
